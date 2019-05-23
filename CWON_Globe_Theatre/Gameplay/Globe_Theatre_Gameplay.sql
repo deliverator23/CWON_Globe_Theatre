418e6b22b90cef0ccc8fa45b9648ed0b1084954b
deliverator23 <iain.millar@gmail.com>
2019-05-23 20:03:12 +0100

    Remove testing SQL.

--- CWON_Globe_Theatre/Gameplay/Globe_Theatre_Gameplay.sql
+++ CWON_Globe_Theatre/Gameplay/Globe_Theatre_Gameplay.sql
@@ -2,9 +2,6 @@
 	Civilization
 	Authors: WitchLychKing
 */
-DROP TRIGGER OnTypeInsert;
-CREATE TRIGGER OnTypeInsert AFTER INSERT ON Types BEGIN UPDATE Types SET Hash = random() Where Type = New.Type; END;
-PRAGMA foreign_keys = ON;
 
 -----------------------------------------------
 -- Types
