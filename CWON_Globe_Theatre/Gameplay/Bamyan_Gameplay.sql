f8cd83ae3aab9d4a5beda9b66c5e9c089e9986a2
iainmillar23 <terramirabilis1>
2019-06-13 17:07:53 +0100

    Update WIP files.

---
+++ CWON_Globe_Theatre/Gameplay/Bamyan_Gameplay.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Types (Type, Kind) VALUES ('BUILDING_BAMYAN', 'KIND_BUILDING');
+
+INSERT INTO Buildings (BuildingType, Name, Description, PrereqCivic, Cost, AdvisorType, MaxWorldInstances, IsWonder, RequiresPlacement, Quote)
+VALUES ('BUILDING_BAMYAN', 'LOC_BUILDING_BAMYAN_NAME', 'LOC_BUILDING_BAMYAN_DESCRIPTION', 'TECH_CURRENCY', 710, 'ADVISOR_CULTURE', '1', 1, 1, 'LOC_BUILDING_BAMYAN_QUOTE');
+
